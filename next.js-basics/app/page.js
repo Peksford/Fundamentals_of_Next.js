9bd1dbd7ec04394ef6d70966ffc18d29defe6c2d
Peksford <130459110+Peksford@users.noreply.github.com>
2026-06-26 13:27:38 +0300

    Update page.js

--- next.js-basics/app/page.js
+++ next.js-basics/app/page.js
@@ -3,6 +3,8 @@ import Link from 'next/link';
 export default function Page() {
   return (
     <main>
+      <h1>Home Page</h1>
+      <p>This is my first next.js page</p>
       <nav>
         <Link href="/about">Go to About Page</Link>
       </nav>
