dede92f69b27c2ed8556311a8950a3da97c4c845
Peksford <130459110+Peksford@users.noreply.github.com>
2026-06-26 13:32:12 +0300

    css styling

--- next.js-basics/app/page.js
+++ next.js-basics/app/page.js
@@ -1,9 +1,10 @@
 import Link from 'next/link';
+import styles from './home/home.module.css';
 
 export default function Page() {
   return (
     <main>
-      <h1>Home Page</h1>
+      <h1 className={styles.title}>Home Page</h1>
       <p>This is my first next.js page</p>
       <nav>
         <Link href="/about">Go to About Page</Link>
